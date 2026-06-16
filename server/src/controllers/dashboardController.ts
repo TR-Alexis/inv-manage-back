0df8f4b2ba3449c05cbd391a34168b8f77cdbdcb
AlexisT <alexisuriel.tr@gmail.com>
2026-06-16 06:15:18 -0700

    dashboard route

---
+++ server/src/controllers/dashboardController.ts
@@ -0,0 +1,59 @@
+import { Request, Response} from "express";
+import { PrismaClient } from '@prisma/client';
+import { SalesSummary, ExpenseByCategory, ExpenseSummary } from '../../generated/prisma/browser';
+
+const prisma = new PrismaClient();
+
+export const getDashboardMetrics = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    try {
+        const popularProducts = await prisma.products.findMany({
+            take: 15,
+            orderBy: {
+                StockQuantity: "desc",
+            },
+        });
+        const salesSummary = await prisma.SalesSummary.findMany({
+            take: 5,
+            orderBy: {
+                date: "desc"
+            },
+        });
+        const purchasesSummary = await prisma.purchasesSummary.findMany({
+            take: 5,
+            orderBy: {
+                date: "desc"
+            },
+        });
+        const expenseSummary = await prisma.expenseSummary.findMany({
+            take: 5,
+            orderBy: {
+                date: "desc"
+            },
+        });
+        const expenseByCategorySummaryRaw = await prisma.expenseByCategory.findMany({
+            take: 5,
+            orderBy: {
+                date: "desc"
+            },
+        });
+        const expenseByCategory = expenseByCategorySummaryRaw.map(
+            (item: ExpenseByCategory) => ({
+                ...item, amount: item.amount.toString()
+            })
+        );
+
+        res.json({
+            popularProducts,
+            salesSummary,
+            purchasesSummary,
+            expenseSummary,
+            expenseByCategory
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieving dashboard metrics" })
+    }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+
 // ROUTE IMPORTS
+import { getDashboardMetrics } from "./controllers/dashboardController";
 
 // CONFIGURATIONS
 dotenv.config();
@@ -18,9 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // ROUTES
-app.get("/hello", (req,res) => {
-    res.send("hello world");
-});
+app.use("/dashboard", getDashboardMetrics);
+
 
 
 // SERVER
