57c18102163a6a9bc834b530781023045284fb91
AlexisT <alexisuriel.tr@gmail.com>
2026-06-16 05:18:05 -0700

    backend

---
+++ server/dist/prisma.config.d.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+declare const _default: {
+    schema: string;
+    migrations: {
+        path: string;
+    };
+    datasource: {
+        url: string;
+    };
+};
+export default _default;
+//# sourceMappingURL=prisma.config.d.ts.map
